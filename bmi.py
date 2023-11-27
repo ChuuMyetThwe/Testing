3addf02df834da8c838bc9416111f0aa8fe722eb
Chuu Myet Thwe <CHUUMYET.22@ichat.sp.edu.sg>
2023-11-28 07:24:18 +0800

    changed return value

--- bmi.py
+++ bmi.py
@@ -5,10 +5,10 @@ def bmi_calculator(weight,height):
     print(bmi)
 
     if (bmi < 18.5):
-        print("Underweight")
+        return -1
     elif (bmi >= 18.5 and bmi <= 25.0):
-        print("Normal weight")
+        return 0
     else:
-        print("Over weight")
+        return 1
 
 bmi_calculator(57, 1.73)
